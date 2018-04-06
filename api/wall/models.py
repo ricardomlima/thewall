5484da4504106e27dfe1aef0f55c83160cfcc718
Ricardo M. Lima <ricardolima89@gmail.com>
2018-04-06 12:47:40 -0300

    Registering wall app & models

--- api/wall/models.py
+++ api/wall/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    message = models.CharField(max_length=200)
+    date_published = models.DateTimeField('date published', auto_now_add=True)
