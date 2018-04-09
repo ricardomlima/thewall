e2e83d608f55ae46d0c3b0240fb303c73dbca4cb
Ricardo M. Lima <ricardolima89@gmail.com>
2018-04-09 20:27:46 -0300

    Author field readonly

--- api/wall/v1/serializers.py
+++ api/wall/v1/serializers.py
@@ -3,7 +3,9 @@ from rest_framework import serializers
 from wall.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
-    
+
+    author = serializers.ReadOnlyField(source='author.name')
+
     class Meta:
         model = Post
         fields = '__all__'
