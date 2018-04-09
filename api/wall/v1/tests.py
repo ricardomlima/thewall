6ea27a0786c4295834bce9bb8ab905db15626277
Ricardo M. Lima <ricardolima89@gmail.com>
2018-04-08 21:22:54 -0300

    Test for post creation

--- api/wall/v1/tests.py
+++ api/wall/v1/tests.py
@@ -22,3 +22,15 @@ class PostTestCase(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['id'], self.post.id)
+
+
+    def test_create_post(self):
+        """
+        Test api post creation
+        """
+        new_post = {'message':'new post message'}
+        create_request = self.client.post(self.endpoint, new_post)
+        response = self.client.get(self.endpoint)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
