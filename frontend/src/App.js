5fb81784eca2a117c87544947aece4832fb1be0c
Ricardo M. Lima <ricardolima89@gmail.com>
2018-04-10 11:14:29 -0300

    Frontend wall html structure for posts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Post extends Component {
+  render(){
+    return (
+      <div className="Post">
+        <div className="Author">
+          Ricardo M. Lima
+        </div>
+        <div className="Message">
+          Another brick in the wall
+        </div>
+      </div>
+    )
+  }
+}
+
+class Wall extends Component {
+  render(){
+    return (
+      <div className="Wall">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to The Wall application</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wall />
     );
   }
 }
