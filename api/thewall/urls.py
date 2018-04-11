a040f69c8909e90fd2dfe1b274832289c7527982
Ricardo M. Lima <ricardolima89@gmail.com>
2018-04-11 19:10:45 -0300

    V1 documentation of API with coreapi

--- api/thewall/urls.py
+++ api/thewall/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/docs/',  include_docs_urls(title='The Wall API V1')),
     path('api/', include('wall.urls')),
     path('api/', include('user.urls')),
 ]
