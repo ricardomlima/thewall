b6521a4805556364c7f3ad77cdf70748a84dc743
Ricardo M. Lima <ricardolima89@gmail.com>
2018-04-12 12:11:04 -0300

    Django rules for custom model username

--- api/user/models.py
+++ api/user/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class WallUser(AbstractUser):
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['']
 
     name = models.CharField(blank=True, max_length=255)
+    email = models.EmailField(blank=False, unique=True)
 
     def __str__(self):
         return self.username
