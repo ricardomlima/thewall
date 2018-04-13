f8b1b885503a843effd4f683f03025f15835677d
Ricardo M. Lima <ricardolima89@gmail.com>
2018-04-12 23:06:42 -0300

    User email verification test

--- api/user/v1/tests.py
+++ api/user/v1/tests.py
@@ -1,3 +1,4 @@
+from allauth.account.models import EmailAddress, EmailConfirmationHMAC
 from rest_framework.test import APITestCase
 from model_mommy import mommy
 
@@ -29,3 +30,36 @@ class AuthTestCase(APITestCase):
 
         r = self.client.post(self.endpoint+'registration/', self.user, format='json')
         self.assertTrue(WallUser.objects.count() > 0)
+
+    def test_user_is_verified_after_email_verification_request(self):
+        """
+        Feauture: User Email Verification
+
+        Given:
+            A user with an Auth token and not verified email
+          AND
+            UserObject.is_verified == False
+          And
+            A correct request with this users token as key parameter
+        When:
+            The request is sent to the email verification endpoint
+        Then:
+            UserObject.is_verified == True
+        """
+
+        # register user
+        res = self.client.post(self.endpoint+'registration/', self.user, format='json')
+        self.assertTrue(WallUser.objects.count() > 0)
+
+        # verify if recently created user's email is not verified
+        wall_user = WallUser.objects.all()[0]
+        self.assertFalse(wall_user.is_verified())
+
+        # get the users key and make the email verification request
+        key = EmailConfirmationHMAC(EmailAddress.objects.filter(user=wall_user).first()).key
+        postData = {'key':key}
+        res = self.client.post(self.endpoint+'registration/verify-email/', postData, format='json')
+
+        # assert if user's email is verified
+        # through the is_verified attribute
+        self.assertTrue(wall_user.is_verified())
