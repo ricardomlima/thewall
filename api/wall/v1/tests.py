3650e5f7b724491a47e4630226f63489ee5c5974
Ricardo M. Lima <ricardolima89@gmail.com>
2018-04-13 11:17:23 -0300

    Describing Post test stories and refactoring

--- api/wall/v1/tests.py
+++ api/wall/v1/tests.py
@@ -16,7 +16,15 @@ class PostTestCase(APITestCase):
     
     def test_list_posts(self):
         """
-        Test api post listing
+        Feature: Post List
+
+        Given:
+            An existent Wall Post in the Database
+        When:
+            I make a GET request to the Post Endpoint
+        Then:
+            I should have a list with the existent Post
+            identified by its primary key
         """
 
         response = self.client.get(self.endpoint)
@@ -27,35 +35,72 @@ class PostTestCase(APITestCase):
         self.assertEqual(json_result['count'], 1)
         self.assertEqual(json_result['results'][0]['id'], self.post.id)
 
-
     def test_create_post(self):
         """
-        Test api post creation
+        Feature: Post creation
+
+        Given:
+            No existent Post
+          AND
+            An existent User
+        When:
+            I authenticate the User
+          AND
+            I make a POST request with a Post object as body
+        Then:
+            I should have a persisted Post object in the
+            database with the attribute values as the request
 
         Force authentication headers on request
         """
 
-        self.client.force_authenticate(user=self.user)
-        new_post = {'message':'new post message'}
+        # delete all existent Post objects
+        Post.objects.all().delete()
 
+        # authenticate as the existent user
+        self.client.force_authenticate(user=self.user)
+
+        # create and make the request for a new Post
+        new_post = {'message':'new post message'}
         create_request = self.client.post(self.endpoint, new_post, format='json')
 
+        # check for 201 created status code
         self.assertEqual(create_request.status_code, 201)
 
-        response = self.client.get(self.endpoint)
+        # check if a Post now exists in the database
+        self.assertTrue(Post.objects.count() > 0)
 
-        self.assertEqual(response.status_code, 200)
-        json_result = response.json()
-        self.assertEqual(json_result['count'], 2)
+        # check if the existent post has the same message
+        # value as the request
+        created_post = Post.objects.all()[0]
+        self.assertEquals(created_post.message, new_post['message'])
 
     def test_create_post_not_authenticated(self):
         """
+        Feature: Post Creation Permissions
+
+        Given:
+            No existent Posts in the database
+          AND
+            An existent User
+        When:
+            I make a POST request with no authentication credentials
+            and a Post object as body
+        Then:
+            I should have no Post objects in the database
+
         Test api post creation without an authenticated user
         """
 
-        new_post = {'message': 'another new post message'}
+        # delete all existent Post objects
+        Post.objects.all().delete()
 
+        # create and make the request for a new Post
+        new_post = {'message':'new post message'}
         create_request = self.client.post(self.endpoint, new_post, format='json')
 
         # checking for forbidden status code
         self.assertEqual(create_request.status_code, 403)
+
+        # check if still there is no Post object in the database
+        self.assertEquals(Post.objects.count(),  0)
